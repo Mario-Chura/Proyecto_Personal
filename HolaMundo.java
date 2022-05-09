17837e1b76c620b8097f5573d1ebe11b317190aa
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 21:53:43 -0500

    Imprimiendo mi nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Mi primer archivo en Git");
         System.out.println("Estructura de datos y algoritmos grupo E");
+        System.out.println("Alumno: Mario Franco Chura Puma");
+
 
 	}
 
