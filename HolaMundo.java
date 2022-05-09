e0fc4461528e9f2844e2a86b5dcdce2f81f24e76
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 21:52:50 -0500

    Linea de impresion 2

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Mi primer archivo en Git");
+        System.out.println("Estructura de datos y algoritmos grupo E");
 
 	}
 
